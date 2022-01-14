587ffa64316a258b841d8faa8f839e2e0d68f28e
ohjiyeon3275 <ojy940716@gmail.com>
2022-01-14 22:46:35 +0900

    readme update

--- README.md
+++ README.md
@@ -2,13 +2,6 @@
 
 <br/>
 
-> CORS
->> dd dd
-> dkfjslk <br/>
-> skldjflk
-> 
-> ---
-> 
-> 
-> CSRF
+
+> https://invited-softball-75b.notion.site/Spring-Security-9e502721e9364d34939bb97bd13eba50
 
--- src/main/java/com/jiyeon/project/controller/AuthController.java
+++ src/main/java/com/jiyeon/project/controller/AuthController.java
@@ -27,6 +27,17 @@ public class AuthController {
         return "log in ed";
     }
 
+    /**
+     *  /auth/login이 403뜬다... 왜????
+     *  /auth/join
+     *
+     *  1. 프론트랑 따로 쓰다가 붙혀 쓰다가 해서 그럴수도
+     *  2. html경로?
+     *  3--유력 : securityConfig를 손봐야한다
+     *
+     *  --> 해결하고나서 구글아이디에서 회원가입하도록 정보 받아와야한다.
+     */
+    
     @PostMapping("/join")
     public @ResponseBody String joinOK(Employee employee){
 
